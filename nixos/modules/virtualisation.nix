18abf458149c04301019e8350f3a561ab0e588f5
Nelson Segarra <junicus@outlook.com>
2025-02-03 17:21:41 -0400

    update to podman settings

--- nixos/modules/virtualisation.nix
+++ nixos/modules/virtualisation.nix
@@ -2,6 +2,19 @@
   virtualisation = {
     podman = {
       enable = true;
+
+      dockerCompat = true;
+      dockerSocket.enable = true;
+
+      defaultNetwork.settings.dns_enabled = true;
     };
+
+    environment.systemPackages = with pkgs; [
+      podman-compose
+      podman-tui
+
+      docker-compose
+      lazidocker
+    ];
   };
 }
